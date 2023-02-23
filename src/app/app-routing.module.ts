2d41ec9b2881d04d28d7b7c407a0da06af38a6ff
felipeofor <felipeofor@gmail.com>
2023-02-23 10:32:58 -0300

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculadoraDeAhorroComponent } from './calculadora-de-ahorro/calculadora-de-ahorro.component';
+import { MillonarioComponent } from './millonario/millonario.component';
+import { GestorDeGastosComponent } from './gestor-de-gastos/gestor-de-gastos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'calculadora-de-ahorro',
+    pathMatch: 'full'
+  },
+  {
+    path: 'calculadoraDeAhorro',
+    component: CalculadoraDeAhorroComponent
+  },
+  {
+    path: 'Millonario',
+    component: MillonarioComponent
+  },
+  {
+    	path: 'GestorDeGastos',
+    component: GestorDeGastosComponent
+},
+{
+  path: 'Contacto',
+  component: ContactoComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

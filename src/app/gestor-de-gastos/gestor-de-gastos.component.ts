04130c698d5e4b463d3629292364c7a6fdd9e894
felipeofor <felipeofor@gmail.com>
2023-02-23 10:40:00 -0300

    localstorage full

--- src/app/gestor-de-gastos/gestor-de-gastos.component.ts
+++ src/app/gestor-de-gastos/gestor-de-gastos.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GestorDeGastosComponent implements OnInit {
 
+  constructor() {
+    if (localStorage.getItem('gastos') !== null) {
+      this.gastos = JSON.parse(localStorage.getItem('gastos') || '[]');
+    }
+  }
+
 
   ngOnInit(): void {
   }
@@ -17,15 +23,12 @@ export class GestorDeGastosComponent implements OnInit {
   inputTipo: string = '';
   obtenerfecha = new Date().getDate() + '/' + (new Date().getMonth() + 1) + '/' + new Date().getFullYear();
   obtenerHora= new Date().getHours() + ':' + new Date().getMinutes();
-
   fechaYHora: string = `${this.obtenerfecha} ${this.obtenerHora}`;
-
   modoEdicion: boolean = false;
-  mostrarTabla: boolean = false;
-  
+
   gastos: any[] = [];
 
-  
+
   agregarGasto(): void{
     if(this.inputDetalle !== '' && this.inputPrecio !== ''){
       this.gastos.push({
@@ -43,13 +46,12 @@ export class GestorDeGastosComponent implements OnInit {
       this.inputPrecio = '';
       this.modoEdicion = false;
     }
-    if(this.gastos.length > 0){
-      this.mostrarTabla = true;
-    }
+    localStorage.setItem('gastos', JSON.stringify(this.gastos));
   }
 
   eliminarGasto(index: number): void{
     this.gastos.splice(index, 1);
+    localStorage.setItem('gastos', JSON.stringify(this.gastos));
   }
 
   editarGasto(index: number): void{
@@ -60,5 +62,6 @@ export class GestorDeGastosComponent implements OnInit {
     this.inputPrecio = this.gastos[index].precio;
     this.gastos.splice(index, 1);
     this.modoEdicion = !this.modoEdicion;
+    localStorage.setItem('gastos', JSON.stringify(this.gastos));
   }
 }
